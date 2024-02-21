f9418f5881e3b2c310589bf531d41f68ef79eb6e
= <=>
2024-02-21 17:55:52 +0530

    added delete post functionality to the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,107 +1,172 @@
 import { useEffect, useState } from "react";
-import { useSelector } from 'react-redux';
-import { Table, TableBody } from 'flowbite-react';
-import { Link } from 'react-router-dom';
-
+import { useSelector } from "react-redux";
+import { Table, TableBody, Modal, Button} from "flowbite-react";
+import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function DashPosts() {
-
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [postIdToDelete, setPostIdToDelete] = useState('');
 
   console.log(userPosts);
 
-  useEffect(()=> {
-     const fetchPosts = async () => {
-        try {
-          const res = await fetch(`api/post/getposts?userId=${currentUser._id}`)
-          const data = await res.json();
-          if(res.ok){
-            setUserPosts(data.posts);
-            if(data.posts.length < 9){
-              setShowMore(false);
-            }
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
           }
-        } catch (error) {
-           
         }
-     }
-     if(currentUser.isAdmin){
-       fetchPosts();
-     }
-  },[currentUser._id]);
+      } catch (error) {}
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
 
   const handleShowMore = async () => {
-      const startIndex = userPosts.length;
-      try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
-        const data = await res.json();
-        if(res.ok){
-            setUserPosts((prev) => [...prev, ...data.posts]);
-            if(data.posts.length < 9){
-              setShowMore(false);
-            }
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
         }
-      } catch (error) {
-        console.log(error)
       }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handelDeletePost = async () => {
+     setShowModal(false);
+     try {
+        const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, {
+          method: 'DELETE'
+        });
+        const data = await res.json();
+        if(!res.ok){
+          console.log(data.message);
+        }else{
+          setUserPosts((prev)=>
+            prev.filter((post) =>post._id !== postIdToDelete)
+          );
+        }
+     } catch (error) {
+       console.log(error)
+     }
   }
 
   return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar
-     scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
-      { currentUser.isAdmin && userPosts.length > 0 ? (
-         <>
-          <Table hoverable className='shadow-md'>
-              <Table.Head>
-                <Table.HeadCell>Date updated</Table.HeadCell>
-                <Table.HeadCell>Post image</Table.HeadCell>
-                <Table.HeadCell>Post title</Table.HeadCell>
-                <Table.HeadCell>Category</Table.HeadCell>
-                <Table.HeadCell>Delete</Table.HeadCell>
-                <Table.HeadCell>
+    <div
+      className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar
+     scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500"
+    >
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            <TableBody className="divide-y">
+              {userPosts.map((post) => (
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      to={`/post/${post.slug}`}
+                      className="font-medium text-gray-900 dark:text-white"
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setPostIdToDelete(post._id);
+                      }}
+                      className="font-medium text-red-500 hover:underline cursor-pointer"
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell className="text-teal-500 hover:underline">
+                    <Link to={`/update-post/${post._id}`}>
                       <span>Edit</span>
-                </Table.HeadCell>
-              </Table.Head>
-              <TableBody className="divide-y">
-              {
-                userPosts.map((post) => (
-                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
-                    <Table.Cell>
-                         <Link to={`/post/${post.slug}`} >
-                            <img 
-                              src = {post.image}
-                              alt = {post.title}
-                              className="w-20 h-10 object-cover bg-gray-500"
-                            />
-                         </Link>
-                    </Table.Cell>
-                    <Table.Cell>
-                      <Link to={`/post/${post.slug}`} className="font-medium text-gray-900 dark:text-white">{post.title}</Link>
-                      </Table.Cell>
-                    <Table.Cell>{post.category}</Table.Cell>
-                    <Table.Cell>
-                        <span className="font-medium text-red-500 hover:underline cursor-pointer">Delete</span>
-                    </Table.Cell>
-                    <Table.Cell className="text-teal-500 hover:underline">
-                        <Link to={`/update-post/${post._id}`} >
-                           <span>Edit</span>
-                        </Link>
-                    </Table.Cell>
-                  </Table.Row>
-                ))
-              }
-              </TableBody>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </TableBody>
           </Table>
-          {
-            showMore && (
-              <button className="w-full text-teal-500 self-center text-sm py-7" onClick={handleShowMore}>Show more</button>
-            )
-          }
-         </>
-      ) : (<p>You have no posts yet!</p>)}
+          {showMore && (
+            <button
+              className="w-full text-teal-500 self-center text-sm py-7"
+              onClick={handleShowMore}
+            >
+              Show more
+            </button>
+          )}
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size="md"
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this post?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handelDeletePost}>
+                Yes I'm sure
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
-  )
+  );
 }
