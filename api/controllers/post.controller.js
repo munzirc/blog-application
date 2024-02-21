f51804f6d1d0c0fb7851c67932351f1064ba1cbd
= <=>
2024-02-21 12:21:42 +0530

    completed create post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -9,11 +9,11 @@ export const create = async (req, res, next) => {
         return next(errorHandler(403, 'You are not allowed to create a post'))
     }
 
-    if(!req.body.title || !req.body.title){
+    if(!req.body.title || !req.body.content){
         return next(errorHandler(403, 'Please provide all required fields'))
     }
 
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    const slug = req.body.title.toLowerCase().trim().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');  
 
     const newPost = new Post({
         ...req.body, 
--- api/models/post.model.js
+++ api/models/post.model.js
@@ -8,7 +8,8 @@ const postSchema = new mongoose.Schema(
         },
         title: {
             type: String,
-            required: true
+            required: true,
+            unique: true
         },
         content:{
             type: String,
