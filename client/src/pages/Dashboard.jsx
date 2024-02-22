65eac08b769f1e846b8657ddf8b65d08df3a6213
= <=>
2024-02-22 15:33:07 +0530

    show users to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 
 export default function Dashboard() {
 
@@ -27,6 +28,8 @@ export default function Dashboard() {
         { tab === 'profile' && <DashProfile /> }
         {/*================================Posts tab================================= */}
         { tab === 'posts' && <DashPosts />}
+        {/* ===============================Users Tab================================= */}
+        { tab === 'users' && <DashUsers />}
     </div>
   )
 }
