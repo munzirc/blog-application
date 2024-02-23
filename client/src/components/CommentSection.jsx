0e0ded7d1c492bebaf015ca0f6cbb704ae2fbb35
= <=>
2024-02-23 21:23:24 +0530

    added edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -82,6 +82,17 @@ export default function CommentSection({ postId }) {
         }
   }
 
+  const handleEdit = async (comment, editedContent) =>{
+    setComments(
+      comments.map((c) => 
+              c._id === comment._id ? {
+                ...c,
+                content: editedContent,
+              } : c
+      ));
+  }
+
+
   return (
     <div className="max-w-3xl mx-auto w-full p-3">
       {currentUser ? (
@@ -143,7 +154,12 @@ export default function CommentSection({ postId }) {
           </div>
           {
           comments.map((comment) => (
-             <Comment key={comment._id} comment={comment} onLike={handleLike} />
+             <Comment 
+             key={comment._id} 
+             comment={comment} 
+             onLike={handleLike} 
+             onEdit={handleEdit}
+             />
           ))}
         </>
 
